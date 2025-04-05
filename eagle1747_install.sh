30ea6a77d267da2a3427e1c2d2627c69a3967acb
ShaheenHossain <rapidgrps@princegroup-bd.com>
2025-04-05 10:55:33 +0600

    Update eagle1747_install.sh

--- eagle1747_install.sh
+++ eagle1747_install.sh
@@ -105,7 +105,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install Eagle
 #--------------------------------------------------
 echo -e "\n==== Installing Eagle Server ===="
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/odoo17ent_mercury $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/ShaheenHossain/odoo17ent_mercury_02 $OE_HOME_EXT/
 
 if [ $IS_ENTERPRISE = "True" ]; then
     # Eagle Enterprise install!
